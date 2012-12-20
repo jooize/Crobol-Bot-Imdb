ae514d03d22da859cdc0002576b007356da6af72
Joel Esko <joel@yuio.se>
2012-12-20 20:17:07 +0100

    Split to functions. Add --no-genre and --no-rating

--- Bot-Imdb.py
+++ Bot-Imdb.py
@@ -4,10 +4,45 @@ import sys
 import argparse
 import imdb
 
+# movie must be an IMDb Movie object
+def get_genre_string(movie):
+	genre_string = movie['genre'][0]
+	for genre in movie['genre'][1:]:
+		genre_string += " | " + genre
+	return genre_string
+
+def get_rating_string(movie, encoding="ASCII"):
+	rating_string = str(movie['rating'])
+	if encoding == "Unicode":
+		# \u2605 is BLACK STAR
+		rating_string += u" \u2605"
+	else:
+		rating_string += " stars"
+	return rating_string
+
+def get_url_string(imdb_access, movie, dash="ASCII"):
+	# Replace akas.imdb.com with imdb.com (will redirect to www.imdb.com)
+	imdb_url = imdb_access.get_imdbURL(movie).replace('http://akas.', 'http://', 1)
+	if dash == "Quotation dash":
+		# \u2015 is HORIZONTAL BAR
+		dash_string = u"\u2015"
+	elif dash == "Em dash":
+		# \u2014 is EM DASH
+		dash_string = u"\u2014"
+	else:
+		dash_string = "--"
+	url_string = dash_string + imdb_url
+	return url_string
+
+def get_title_string(movie):
+	return movie['long imdb canonical title']
+
 def main(argv):
-	parser = argparse.ArgumentParser(description='IMDb stuff.')
+	parser = argparse.ArgumentParser(description='Get information about movies or series from IMDb.')
 	parser.add_argument("title", nargs="+", help="Movie or TV serie")
 	parser.add_argument("--url", help="Print URL to IMDb page", action="store_true")
+	parser.add_argument("--no-genre", dest="genre", help="Don't show genre", action="store_false", default=True)
+	parser.add_argument("--no-rating", dest="rating", help="Don't show rating", action="store_false", default=True)
 	args = parser.parse_args()
 
 	imdb_access = imdb.IMDb()
@@ -17,17 +52,18 @@ def main(argv):
 	search_result = imdb_access.search_movie(movie_args)
 	movie = search_result[0]
 	imdb_access.update(movie)
-	# \u2605 is Black Star
-	print_string = movie['long imdb canonical title'] + " " + str(movie['rating']) + u" \u2605"
-	genre_string = movie['genre'][0]
-	for genre in movie['genre'][1:]:
-		genre_string += " | " + genre
-	print_string += "  " + genre_string
+
+	print_string = get_title_string(movie)
+
+	if args.rating:
+		print_string += " " + get_rating_string(movie, "Unicode")
+
+	if args.genre:
+		print_string += "  " + get_genre_string(movie)
+
 	if args.url:
-		# Replace akas.imdb.com with imdb.com (will redirect to www.imdb.com)
-		imdb_url = imdb_access.get_imdbURL(movie).replace('http://akas.', 'http://', 1)
-		# \u2015 is HORIZONTAL BAR, quotation dash. Use "--" if ASCII is required.
-		print_string += u" \u2015 " + imdb_url
+		print_string += get_url_string(imdb_access, movie, "Quotation dash")
+
 	print print_string
 
 if __name__ == "__main__":
