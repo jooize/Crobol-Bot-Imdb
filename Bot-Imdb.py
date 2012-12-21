652fa8912422c3cbb600a34dfc3fb735bdb231fc
Joel Esko <joel@yuio.se>
2012-12-21 20:46:49 +0100

    Move formatting and spaces from main() to related function for get_*_string()

--- Bot-Imdb.py
+++ Bot-Imdb.py
@@ -2,6 +2,7 @@
 
 import argparse
 import imdb
+import locale
 
 # movie must be an IMDb Movie object
 def get_genre_string(movie):
@@ -11,7 +12,7 @@ def get_genre_string(movie):
 			genre_string += " | " + genre
 	else:
 		genre_string = "No genre"
-	return genre_string
+	return " " + genre_string
 
 def get_rating_string(movie, rating_star="ASCII"):
 	if movie.has_key('rating'):
@@ -23,7 +24,15 @@ def get_rating_string(movie, rating_star="ASCII"):
 			rating_string += " stars "
 	else:
 		rating_string = ""
-	return rating_string
+	return " " + rating_string
+
+def get_votes_string(movie):
+	if movie.has_key('votes'):
+		locale.setlocale(locale.LC_NUMERIC, "")
+		votes_string = "(" + str(locale.format('%d', movie['votes'], True)) + " votes)"
+	else:
+		votes_string = ""
+	return " " + votes_string
 
 def get_url_string(imdb_access, movie, dash="ASCII"):
 	# Replace akas.imdb.com with imdb.com (will redirect to www.imdb.com)
@@ -37,7 +46,7 @@ def get_url_string(imdb_access, movie, dash="ASCII"):
 	else:
 		dash_string = "--"
 	url_string = dash_string + " " + imdb_url
-	return url_string
+	return " " + url_string
 
 def get_title_string(movie, quotes="Double quotation marks"):
 	if quotes == "Directional double quotation marks":
@@ -50,14 +59,26 @@ def get_title_string(movie, quotes="Double quotation marks"):
 
 # Returns "????" if no year
 def get_year_string(movie):
-	return str(movie['year'])
+	year_string = "(" + str(movie['year']) + ")"
+	return " " + year_string
 
 def get_runtime_string(movie):
 	if movie.has_key('runtime'):
 		runtime_string = "(" + str(movie['runtime'][0]) + " min)"
 	else:
 		runtime_string = "(?? min)"
-	return runtime_string
+	return " " + runtime_string
+
+def get_plot_string(movie, quotes="Double quotation marks"):
+	if movie.has_key('plot'):
+		plot_string = movie['plot'][0].rsplit('::', 1)[0]
+		if quotes == "Directional double quotation marks":
+			plot_string = u"\u201C" + plot_string + u"\u201D"
+		else:
+			plot_string = '"' + plot_string + '"'
+	else:
+		plot_string = "No plot found."
+	return " " + plot_string
 
 def main():
 	parser = argparse.ArgumentParser(usage="!imdb [-huAGRY] [--url] title [{title,year} ...]", description='Get information about movies or series from IMDb.')
@@ -67,8 +88,9 @@ def main():
 	parser.add_argument("-R", "--no-rating", dest="rating", help="Don't show rating", action="store_false", default=True)
 	parser.add_argument("-Y", "--no-year", dest="year", help="Don't show year", action="store_false", default=True)
 	parser.add_argument("-A", "--ascii-only", help="Only ASCII, no Unicode", action="store_true")
-	#parser.add_argument("-p", "--plot", help="Show plot", action="store_true")
+	parser.add_argument("-p", "--plot", help="Show plot", action="store_true")
 	parser.add_argument("--runtime", help="Show runtime", action="store_true")
+	parser.add_argument("--votes", help="Show votes", action="store_true")
 	args = parser.parse_args()
 
 	imdb_access = imdb.IMDb()
@@ -92,25 +114,34 @@ def main():
 		print_string = get_title_string(movie, "Directional double quotation marks")
 
 	if args.year:
-		print_string += " (" + get_year_string(movie) + ")"
+		print_string += get_year_string(movie)
 
 	if args.rating:
 		if args.ascii_only:
-			print_string += " " + get_rating_string(movie)
+			print_string += get_rating_string(movie)
 		else:
-			print_string += " " + get_rating_string(movie, "Unicode")
+			print_string += get_rating_string(movie, "Unicode")
+	
+	if args.votes:
+		print_string += get_votes_string(movie)
 
 	if args.genre:
-		print_string += " " + get_genre_string(movie)
+		print_string += get_genre_string(movie)
 
 	if args.runtime:
-		print_string += " " + get_runtime_string(movie)
+		print_string += get_runtime_string(movie)
 	
 	if args.url:
 		if args.ascii_only:
-			print_string += " " + get_url_string(imdb_access, movie)
+			print_string += get_url_string(imdb_access, movie)
 		else:
-			print_string += " " + get_url_string(imdb_access, movie, "Quotation dash")
+			print_string += get_url_string(imdb_access, movie, "Quotation dash")
+
+	if args.plot:
+		if args.ascii_only:
+			print_string += get_plot_string(movie)
+		else:
+			print_string += get_plot_string(movie, "Directional double quotation marks")
 
 	print print_string
 
